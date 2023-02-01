b6faecd515ac4a8c45f74528b4d939b6d9e50402
Yogeshw007 <yogilal97@gmail.com>
2023-02-01 16:34:40 +0530

    Updated logic for specific status API

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -5,8 +5,14 @@ module.exports.reports = async function (req, res) {
     let status = req.params.status;
     let patients = await Patient.find({ status });
 
+    let patientsWithSpecificStatus = patients.map((patient) => {
+        if (patient.status[patient.status.length - 1] === status) {
+            return patient.name;
+        }
+    })
+
     return res.json(200, {
         message: `Patients with status ${status}`,
-        patients: patients
+        patients: patientsWithSpecificStatus
     })
 }
